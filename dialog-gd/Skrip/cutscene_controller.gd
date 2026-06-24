710ce3e57389a24edf3c99d4d86ac1c87583b6d6
azizul987 <wawanajah954@gmail.com>
2026-06-24 14:52:47 +0700

    add cutscne controler

--- dialog-gd/Skrip/cutscene_controller.gd
+++ dialog-gd/Skrip/cutscene_controller.gd
@@ -1,25 +1,39 @@
 extends Node
 
-@onready var luffy = $"../world/Luffy"
+@onready var Luffy = $"../world/Luffy"
 @onready var Aster = $"../world/Aster"
 @onready var dialog_manager = $"../dialogmanager"
-@onready var titik_luffy1 =$"../Luffy titik/Titik1"
-@onready var titik_luffy2 =$"../Luffy titik/Titik2"
-
+@onready var titik_Luffy1 = $"../Luffy titik/Titik1"
+@onready var titik_Luffy2 = $"../Luffy titik/Titik2"
+@onready var titik_Aster1 = $"../Aster Node/marker"
+@onready var titik_Aster2 = $"../Aster Node/marker2"
 func mulai_cutscene() -> void:
-	# NPC Luffy mulai berjalan ke Marker2D
-	luffy.look_right()
-	luffy.walk_to(titik_luffy1.global_position)
+	# Luffy berjalan ke titik pertama.
+	Luffy.look_right()
+	Luffy.walk_to(titik_Luffy1.global_position)
 
-	# Menunggu sampai Luffy benar-benar tiba
-	await luffy.wait_until_arrived()
-	luffy.look_up()
-	luffy.walk_to(titik_luffy2.global_position)
+	await Luffy.wait_until_arrived()
 
-	await  luffy.wait_until_arrived()
-	luffy.get_child(3).show()
-	# Menampilkan dialog
-	dialog_manager.mulai_dialog("start")
+	# Luffy berjalan ke titik kedua.
+	Luffy.look_up()
+	Luffy.walk_to(titik_Luffy2.global_position)
 
-	# Menunggu dialog selesai
-	await dialog_manager.dialog_selesai
+	await Luffy.wait_until_arrived()
+
+	# Misalnya child ke-4 dimunculkan.
+	Luffy.get_child(3).show()
+
+	# Dialog bagian pertama.
+	await dialog_manager.mulai_dialog("start")
+	# Contoh menyembunyikan child ke-4.
+	Luffy.get_child(3).hide()
+	Aster .walk_to(titik_Aster1.global_position)
+	await  Aster.wait_until_arrived()
+	Aster.walk_to(titik_Aster2.global_position)
+	await  Aster.wait_until_arrived()
+	Aster.get_child(2).show()
+	# Dialog bagian kedua.
+	await dialog_manager.mulai_dialog("wifi")
+
+	# Aksi berikutnya setelah dialog kedua selesai.
+	Aster.look_up()
--- dialog-gd/Skrip/dialog_manager.gd
+++ dialog-gd/Skrip/dialog_manager.gd
@@ -3,17 +3,112 @@ extends Node
 signal dialog_selesai
 
 @export var dialog_data: data_dialog
-@onready var ui:CanvasLayer = $"../ui"
-@onready var label_penutur :Label = $"../ui/Dialog/Name/name"
-@onready var label_narasi :Label = $"../ui/Dialog/Isi/dialog_txt"
+
+@onready var dialog_box: Control = $"../ui/Dialog"
+@onready var label_penutur: Label = $"../ui/Dialog/Name/name"
+@onready var label_narasi: Label = $"../ui/Dialog/Isi/dialog_txt"
+
+@onready var luffy: CharacterBody2D = $"../world/Luffy"
+@onready var aster: CharacterBody2D = $"../world/Aster"
 
 var id_awal: String = "start"
+var dialog_aktif: bool = false
+var node_sekarang: node_dialog
+
 
 func _ready() -> void:
-	ui.hide()
-func mulai_dialog(id_dialog: String) -> void:
-	ui.show()
-	
-	await get_tree().create_timer(2.0).timeout
-	
+	dialog_box.hide()
+	sembunyikan_semua_tanda()
+
+
+func mulai_dialog(id_dialog: String = "") -> void:
+	if dialog_aktif:
+		return
+
+	if id_dialog.is_empty():
+		id_dialog = id_awal
+
+	dialog_aktif = true
+	dialog_box.show()
+
+	tampilkan_dialog(id_dialog)
+
+	await dialog_selesai
+
+
+func tampilkan_dialog(id_dialog: String) -> void:
+	node_sekarang = cari_node_dialog(id_dialog)
+
+	if node_sekarang == null:
+		push_warning("Dialog dengan ID '" + id_dialog + "' tidak ditemukan.")
+		selesai_dialog()
+		return
+
+	label_penutur.text = node_sekarang.penutur
+	label_narasi.text = node_sekarang.narasi
+
+	atur_tanda_penutur(node_sekarang.penutur)
+
+
+func cari_node_dialog(id_dialog: String) -> node_dialog:
+	for node in dialog_data.nodes:
+		if node == null:
+			continue
+
+		if node.id.strip_edges() == id_dialog.strip_edges():
+			return node
+
+	return null
+
+
+func _input(event: InputEvent) -> void:
+	if not dialog_aktif:
+		return
+
+	if event.is_action_pressed("ui_accept"):
+		lanjut_dialog()
+
+
+func lanjut_dialog() -> void:
+	var id_tujuan: String = node_sekarang.id_selanjutnya.strip_edges()
+
+	if id_tujuan.is_empty():
+		selesai_dialog()
+		return
+
+	tampilkan_dialog(id_tujuan)
+
+
+func selesai_dialog() -> void:
+	dialog_box.hide()
+	dialog_aktif = false
+
+	sembunyikan_semua_tanda()
+
 	dialog_selesai.emit()
+
+
+func atur_tanda_penutur(nama_penutur: String) -> void:
+	sembunyikan_semua_tanda()
+
+	match nama_penutur.strip_edges().to_lower():
+		"luffy":
+			ubah_tanda(luffy, true)
+
+		"aster":
+			ubah_tanda(aster, true)
+
+		_:
+			pass
+
+
+func sembunyikan_semua_tanda() -> void:
+	ubah_tanda(luffy, false)
+	ubah_tanda(aster, false)
+
+
+func ubah_tanda(karakter: Node, tampil: bool) -> void:
+	var tanda := karakter.get_node_or_null("TandaDialog") as CanvasItem
+
+	if tanda != null:
+		tanda.visible = tampil
